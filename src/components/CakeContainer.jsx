f9446b5a4b6b17ad918faf04ee30ea900b2ff0df
aramix <aram1988slm@gmail.com>
2025-02-20 09:50:05 +0330

    cakeContainer is added

--- src/components/CakeContainer.jsx
+++ src/components/CakeContainer.jsx
@@ -1,7 +1,18 @@
+import { useDispatch, useSelector } from "react-redux";
+import { BUY_CAKE } from "../redux/cake/cakeTypes";
+
 function CakeContainer() {
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
   return (
     <div>
-      <h2>cake numbers</h2>
+      <h2>cake numbers({state.numOfCakes})</h2>
+      <button onClick={() => dispatch({ type: BUY_CAKE, payLoad: 1 })}>
+        buy one cake
+      </button>
+      <button onClick={() => dispatch({ type: BUY_CAKE, payLoad: 2 })}>
+        buy two cakes
+      </button>
     </div>
   );
 }
